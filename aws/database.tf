1ef90a6614809a57cc7e051cd7ab781703589337
Alexandre Silva <alexandre@youcanbook.me>
2022-08-16 21:15:10 +0100

    Update postgres version

--- aws/database.tf
+++ aws/database.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "finances-rds" {
   identifier                      = "finances-db"
   engine                          = "postgres"
-  engine_version                  = "13.3"
+  engine_version                  = "13.4"
   allocated_storage               = 10
   max_allocated_storage           = 20
   instance_class                  = "db.t3.micro"
