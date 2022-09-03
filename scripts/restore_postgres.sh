59df41554c59906088f8fef2590e2fa4a0174724
Alexandre Silva <alexandre@youcanbook.me>
2022-09-03 14:27:54 +0100

    Addng variables to restore local db

--- scripts/restore_postgres.sh
+++ scripts/restore_postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-export HOST=
-export USER=
-export DB=
+export HOST=localhost
+export USER=postgres
+export DB=finances
 psql -h $HOST -p 5432 -U $USER -W -d $DB -c 'TRUNCATE app_users, category, income, expense, budget, budget_periods, budget_categories, savings, databasechangelog, databasechangeloglock'
-pg_restore -h $HOST -p 5432 -U $USER -W -d $DB -a -O -v latest.dump
+pg_restore -h $HOST -p 5432 -U $USER -W -d $DB -a -O -v finances.dump
